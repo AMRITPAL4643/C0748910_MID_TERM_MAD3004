7a6b09d1616756c398e82d7f100796acb771ce22
MacStudent <macstudent@126-imac14.local>
2019-02-20 19:27:21 -0500

    objects are created

---
+++ C0748910_MID_TERM_MAD3004/Extension.swift
@@ -0,0 +1,18 @@
+//
+//  Extension.swift
+//  C0748910_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2019-02-20.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+extension Int{
+    func currency() ->String{
+        return "$ \(self)"
+    }
+}
+
+var productName : String!
+var price : Double!
+var quantity : Int!
--- C0748910_MID_TERM_MAD3004/Product.swift
+++ C0748910_MID_TERM_MAD3004/Product.swift
@@ -7,19 +7,36 @@
 //
 
 import Foundation
+
 class Product:Manufacturer
 {
-    var productID: String!
-    var productName: String!
-    var price: Double!
-    var quantity: Int!
-   override init()
+ 
+    var productID: String
+    var productName: String
+    var price: Double
+    var quantity: Int
+    override init()
     {
-        super.init()
+        
         self.productID = String()
         self.productName = String()
         self.price = Double()
         self.quantity = Int()
+    }
+    
+    init(productID:String, productName:String, price:Double, quantity:Int)
+    {
+        
+        self.productID = productID
+        self.productName = productName
+        self.price = price
+        self.quantity = quantity
+    }
+    func displayData() {
+        print("Product ID: \(self.productID)")
+        print("Product Name: \(self.productName)")
+        print("Product Price: \(self.price)")
+        print("Product Quantity: \(self.quantity)\n")
     }
 }
 
